398f65a815c68d5ac7d988bff3c9251ad8ea45e6
Luis Encina <luisencina369@gmail.com>
2022-05-30 21:44:28 -0400

    add alias in webpack, images and icons link has been repared and hooks use init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import '../styles/Header.scss';
+import React, { useState } from 'react';
+import '@styles/Header.scss';
+import Menu from '@components/Menu'
+import icon_menu from '@icons/icon_menu.svg'
+import icon_cart from '@icons/icon_shopping_cart.svg'
+import logo from '@logos/logo_yard_sale.svg'
 
 const Header = () => {
+
+	const [toogle, setToogle] = useState(false)
+
+	const handleToogle = () => {
+		setToogle(!toogle)
+	}
+
 	return (
 		<nav>
-			<img src="./icons/icon_menu.svg" alt="menu" className="menu" />
+			<img src={icon_menu} alt="menu" className="menu" />
 			<div className="navbar-left">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<img src={logo} alt="logo" className="nav-logo" />
 				<ul>
 					<li>
 						<a href="/">All</a>
@@ -30,13 +41,16 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email">platzi@example.com</li>
+					<li className="navbar-email" onClick={handleToogle}>
+						platzi@example.com
+					</li>
 					<li className="navbar-shopping-cart">
-						<img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
+						<img src={icon_cart} alt="shopping cart" />
 						<div>2</div>
 					</li>
 				</ul>
 			</div>
+			{toogle && <Menu />}
 		</nav>
 	);
 }
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import OrderItem from '../components/OrderItem';
 import '../styles/MyOrder.scss';
 
+import icon_flechita from '@icons/flechita.svg'
+
 const MyOrder = () => {
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
-				<img src="./icons/flechita.svg" alt="arrow" />
+				<img src={icon_flechita} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
